d6a22ccafff2c5df55ee18f5b60d0b5a3cce136b
Dmitriy <d_kurban@yahoo.com>
2023-02-12 15:36:01 +1000

    draft ex_64

--- ex_64/Program.cs
+++ ex_64/Program.cs
@@ -2,5 +2,25 @@
 числа в промежутке от N до 1. Выполнить с помощью рекурсии.
 
 N = 5 -> "5, 4, 3, 2, 1"
-N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" 
+
+5
+4=5-1 n-1
+3 = 4-1
+2 = 3-1
+1 = 2-1
+
+
+*/
+
+int Func (int num)
+{
+    if ()
+    int prev = num - 1;
+    return prev;
+}
+
+Console.WriteLine("Insert nuber");
+int N = Int32.Parse(Console.ReadLine());
+
 
