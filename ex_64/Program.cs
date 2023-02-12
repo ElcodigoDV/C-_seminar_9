418055a671adb2e605dad347579a63f8fc43e409
DmitriyK <d_kurban@yahoo.com>
2023-02-12 16:35:43 +1000

    ex_64 is done

--- ex_64/Program.cs
+++ ex_64/Program.cs
@@ -2,25 +2,29 @@
 числа в промежутке от N до 1. Выполнить с помощью рекурсии.
 
 N = 5 -> "5, 4, 3, 2, 1"
-N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" 
-
-5
-4=5-1 n-1
-3 = 4-1
-2 = 3-1
-1 = 2-1
-
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
 */
 
-int Func (int num)
-{
-    if ()
-    int prev = num - 1;
-    return prev;
+int Func(int num)
+{   
+    if (num > 1)
+    {
+        Console.Write($"{ num}, ");
+        int result = num - 1;
+        return Func(result);
+    }
+    else
+    {
+        Console.Write($"{num}\"");
+        return 1;
+    }
 }
 
 Console.WriteLine("Insert nuber");
 int N = Int32.Parse(Console.ReadLine());
 
-
+// int prev = Func(N);
+Console.Write($"N = {N} -> \"");
+int prev = Func(N);
+Console.WriteLine();
